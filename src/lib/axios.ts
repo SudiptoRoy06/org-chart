acaad6b4105688eef037047564bf2b954425ef83
SudiptoRoy06 <1993sroy@gmail.com>
2025-12-16 22:33:53 +0530

    error handleing

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+if (!baseURL) {
+  throw new Error("NEXT_PUBLIC_API_BASE_URL is not defined");
+}
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL,
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
   },
