dc2ce599078cf1fde566b066f91987af8ac94688
SudiptoRoy06 <1993sroy@gmail.com>
2025-12-16 22:05:12 +0530

    .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const TOKEN = process.env.NEXT_PUBLIC_AUTH_TOKEN || "";
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
-    Authorization: `Bearer ${TOKEN}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTH_TOKEN}`,
   },
 });
