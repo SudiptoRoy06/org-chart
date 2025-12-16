0b791e816dbfdb8de2824f73aad48784173b4bb2
SudiptoRoy06 <1993sroy@gmail.com>
2025-12-16 22:08:06 +0530

    console log test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 export default function Home() {
+
+  console.log("API URL:", process.env.NEXT_PUBLIC_API_BASE_URL);
+  console.log("TOKEN EXISTS:", !!process.env.NEXT_PUBLIC_AUTH_TOKEN);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
